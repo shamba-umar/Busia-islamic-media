e1c4dabca33ca6940e18a5a1c1901750b0fcdcdd
Shamba Abed Umar <108613697+shamba-umar@users.noreply.github.com>
2023-02-17 17:29:40 +0300

    git commit

--- 2-form.php
+++ 2-form.php
@@ -23,20 +23,3 @@
     </form>
   </body>
 </html>
-
-<?php
-if(isset($_POST['submit'])){
-// Fetching variables of the form which travels in URL
-$email = $_POST['email'];
-$name = $_POST['name'];
-$message = $_POST['message'];
-if($email !=''&& $name !=''&& $message !='')
-{
-//  To redirect form on a particular page
-header("Location:https://index.html");
-}
-else{
-?><span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
-}
-}
-?>
