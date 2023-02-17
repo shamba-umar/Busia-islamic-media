478ae02a5a4d5ff1e2b880d54b1ffd46e07cadf0
Shamba Abed Umar <108613697+shamba-umar@users.noreply.github.com>
2023-02-17 17:40:43 +0300

    git commit

---
+++ redirect.php
@@ -0,0 +1,16 @@
+<?php
+if(isset($_POST['submit'])){
+// Fetching variables of the form which travels in URL
+$email = $_POST['email'];
+$name = $_POST['name'];
+$message = $_POST['message'];
+if($email !=''&& $name !=''&& $message !='')
+{
+//  To redirect form on a particular page
+header("Location:https://github.com/shamba-umar/Busia-islamic-media/");
+}
+else{
+?><span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
+}
+}
+?>
---
+++ redirect_form.php
@@ -0,0 +1,31 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Redirect Form To a Particular Page On Submit - Demo Preview</title>
+<meta content="noindex, nofollow" name="robots">
+<link href='css/redirect_form.css' rel='stylesheet' type='text/css'> <!--== Include CSS File Here ==-->
+</head>
+<body>
+<div class="main">
+<div class="first">
+<h2>Redirect Form To a Particular Page On Submit using PHP</h2>
+<form action="redirect_form.php" id="#form" method="post" name="#form">
+
+<label>Email :</label>
+<input id="email" name="email" placeholder='Valid Email Address' type='text'>
+<label>Name :</label>
+<input id="name" name="name" placeholder='name' type='text'>
+<label>message:</label>
+<input id="message" name="message" placeholder='message' type='text' value="">
+<input id='btn' name="submit" type='submit' value='Submit'>
+<!---- Including PHP File Here ---->
+<?php
+include "include/2-form.php";
+?>
+</form>
+</div>
+</div>
+</body>
+</html>
+
