8b5bae8babf9689f58b6db730783fc089a0f6184
Shamba Abed Umar <108613697+shamba-umar@users.noreply.github.com>
2023-02-17 18:21:54 +0300

    git commit

--- redirect.php
+++ redirect.php
@@ -7,7 +7,7 @@ $message = $_POST['message'];
 if($email !=''&& $name !=''&& $message !='')
 {
 //  To redirect form on a particular page
-header("Location:https://github.com/shamba-umar/Busia-islamic-media/");
+header("Location:index.html");
 }
 else{
 ?><span><?php echo "Please fill all fields.....!!!!!!!!!!!!";?></span> <?php
